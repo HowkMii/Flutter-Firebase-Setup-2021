9d36eeaf4fb4e6d4d6d8354c2d7cc7abcb4384c7
howkmii <abdelhakim@univ-tiaret.dz>
2021-12-11 14:43:29 +0100

    main

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,16 @@ class _MyHomePageState extends State<MyHomePage> {
           builder:
               (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (!snapshot.hasData) return const SizedBox.shrink();
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                final docData = snapshot.data!.docs[index].data();
+                final dateTime = (docData['timestamp'] as Timestamp).toDate();
+                return ListTile(
+                  title: Text(dateTime.toString()),
+                );
+              },
+            );
           },
         ));
   }
